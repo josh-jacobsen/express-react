416ab0cd2904f0c924811df027e2fd17d616d31f
josh-jacobsen <joshjacobsen5@gmail.com>
2018-08-18 20:15:39 +1200

    update port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT;
 // app.get('/', (req, res) => res.send("Hello there"))
 
 
