b000fd25686a69f7b07529b008b291721864b349
josh-jacobsen <joshjacobsen5@gmail.com>
2018-08-18 19:51:56 +1200

    front + backend talking

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {foods: []}
+  state = {passwords: []}
 
   componentDidMount(){
-    fetch('/food')
+    this.getPasswords();
+  }
+
+  getPasswords = () => {
+    // Get the passwords and store them in state
+    console.log("getting passwords")
+    fetch('/api/passwords')
       .then(res => res.json())
-      .then(foods => this.setState({ foods }));
+      .then(passwords => this.setState({ passwords }));
   }
   render() {
+    const { passwords } = this.state;
     return (
       <div className="App">
-        <h1>Foods</h1>
-        {this.state.foods.map(food =>
-          <div key={food.id}>{food.itemName}: ${food.price}</div>
-        )}
+        <h1>Random number generator</h1>
+        {passwords.map((password, index) =>
+                <li key={index}>
+                  {password}
+                </li>
+              )}
+        <button
+          className="more"
+          onClick={this.getPasswords}>
+          Get Again
+        </button>
       </div>
+      
     );
   }
 }
--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express = require('express')
 const app = express()
+var router = express.Router();
+const generatePassword = require('password-generator');
 
 const path = require('path');
 
-
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => res.send("Hello there"))
+// app.get('/', (req, res) => res.send("Hello there"))
 
-app.get('/food', (req, res) => res.json(
-    [
-        {
-            id: 1,
-            itemName: "French Fries",
-            price: 3.50
-        }, {
-            id: 2,
-            itemName: "Hamburger",
-            price: 6.50
-        }
-    ]
-));
+
+app.get('/api/passwords', (req, res) => {
+
+    const count = 5;
+
+    // Generate some passwords
+    const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+    )
+
+    res.json(passwords)
+});
+
 
 app.listen(4000, () => console.log('Example app listening on port 4000!'))
 
